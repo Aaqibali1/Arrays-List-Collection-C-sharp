3ac01fe19e6d0fa365b1498fc422afbae3769dfc
Aaqib Ali <75785706+Aaqibali1@users.noreply.github.com>
2023-07-12 00:37:16 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string myName = "Aaqib Ali";
+        Console.WriteLine(myName[3]);
+        Console.WriteLine(myName.IndexOf("a"));
+        Console.WriteLine(myName.Length);
+    }
+}
